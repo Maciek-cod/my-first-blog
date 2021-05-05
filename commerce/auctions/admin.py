7823c2862f759992cc46b4072105c0ebdc82a33e
maciek <sylwestrowiczmaciek8@gmail.com>
2021-05-05 04:39:03 +0100

    app deleted

--- commerce/auctions/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Listing, Bid, Comment, Watchlist, User
-
-# Register your models here.
-admin.site.register(Listing)
-admin.site.register(Bid)
-admin.site.register(Comment)
-admin.site.register(Watchlist)
-admin.site.register(User)
